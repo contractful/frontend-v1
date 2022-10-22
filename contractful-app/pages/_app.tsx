57ffed3e0080787cafe1a676173de7379a6f5764
Mijail Piekarz <piekarzmijail@gmail.com>
2022-10-21 23:57:40 -0300

    Add basic connectWalletButton component
    - Add wagmi config
    - Add rainbow kit config

--- contractful-app/pages/_app.tsx
+++ contractful-app/pages/_app.tsx
@@ -1,7 +1,35 @@
+import "@rainbow-me/rainbowkit/styles.css";
 import type { AppProps } from "next/app";
 
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Contractful",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains} modalSize="compact">
+        <Component {...pageProps} />;
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
 
 export default MyApp;
