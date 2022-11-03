69445a4e4bba1f4df9ed9d391fe22332947cc37c
Mijail Piekarz <piekarzmijail@gmail.com>
2022-11-03 14:11:17 -0300

    Make the action section of the review page dynamic
    Step 3 of the stepper on the review page has been moved to a different component that is rendered conditionally depending on who accesses the page.

---
+++ contractful-app/components/Review/ActionSection.tsx
@@ -0,0 +1,259 @@
+import { NotInterested } from "@mui/icons-material";
+import BrushIcon from "@mui/icons-material/Brush";
+import {
+  Alert,
+  AlertTitle,
+  Box,
+  Button,
+  Grid,
+  List,
+  ListItem,
+  Stack,
+  StepContent,
+  StepLabel,
+  Typography,
+} from "@mui/material";
+import { BigNumber } from "ethers";
+import { useAccount } from "wagmi";
+import { AgreementParams } from "../../../utils/types";
+
+type Props = {
+  agreementParameters: AgreementParams;
+  challengeDuration: BigNumber;
+  confirmOpen:
+    | {
+        dialogTitle: string;
+        dialogBody: string;
+        cancelCaption: string;
+        okCaption: string;
+        okFunc: () => {};
+      }
+    | undefined;
+  confirmDialogOk: () => void;
+  confirmDialogCancel: () => void;
+  showConfirmDialog: ({}) => void;
+  handleConsentToAgreement: () => void;
+};
+
+const ActionSection = (props: Props) => {
+  const {
+    agreementParameters,
+    challengeDuration,
+    confirmOpen,
+    confirmDialogOk,
+    confirmDialogCancel,
+    showConfirmDialog,
+    handleConsentToAgreement,
+  } = props;
+
+  const { address } = useAccount();
+
+  console.log("confirmOpen", confirmOpen);
+
+  if (!agreementParameters) return <></>;
+  const { activationDate, contractor } = agreementParameters;
+
+  if (activationDate.toString() == "0" && address == contractor) {
+    return (
+      <>
+        <StepLabel>{"Consent to the Hiring Agreement"}</StepLabel>
+        <StepContent>
+          <Grid container pt={2}>
+            <Grid item xs={2}></Grid>
+            <Grid item xs>
+              <Alert severity="info">
+                <AlertTitle>Please review the Agreement carefully.</AlertTitle>
+                If all details are correct, you can continue to consent to the
+                Agreement.
+                <br />
+              </Alert>
+              {
+                <Stack pt={2} spacing={2}>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    startIcon={<BrushIcon />}
+                    fullWidth
+                    onClick={() => {
+                      showConfirmDialog({
+                        dialogTitle:
+                          "Are you sure you want to consent to the Hiring Agreement?",
+                        dialogBody:
+                          "The underlying Smart Contract becomes active by consenting to the Agreement. The Agreement is binding for both parties.",
+                        cancelCaption: "Cancel",
+                        okCaption: "Consent to Agreement",
+                        okFunc: () => {
+                          handleConsentToAgreement();
+                        },
+                      });
+                    }}
+                  >
+                    Consent to the Agreement
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="error"
+                    size="large"
+                    startIcon={<NotInterested />}
+                    fullWidth
+                    onClick={() => {
+                      showConfirmDialog({
+                        dialogTitle:
+                          "Are you sure you want to decline the Hiring Agreement?",
+                        dialogBody:
+                          "Declining the Hiring Agreement invalidates the Smart Contract to which the budget has been transfered. Be very sure you really want to decline the Hiring Agreement.",
+                        cancelCaption: "Cancel",
+                        okCaption: "Really decline Agreement",
+                        okFunc: () => {
+                          console.log("DEBUG In okFunc");
+                        },
+                      });
+                    }}
+                  >
+                    Decline Agreement
+                  </Button>
+                </Stack>
+              }
+            </Grid>
+            <Grid item xs={3}></Grid>
+          </Grid>
+        </StepContent>
+      </>
+    );
+  }
+
+  const { maturityDate, paymentCycleDuration, contractee } =
+    agreementParameters;
+
+  const agreementDuration = maturityDate.sub(activationDate);
+  const migrations = agreementDuration.div(paymentCycleDuration);
+
+  let validChallengePeriod = false;
+  const reminder =
+    agreementDuration.mod(paymentCycleDuration).toString() !== "0";
+  for (let i = 0; i < migrations.toNumber(); i++) {
+    const migrationPeriod = activationDate.add(
+      paymentCycleDuration.mul(BigNumber.from(i + 1))
+    );
+
+    if (BigNumber.from(Date.now()) >= migrationPeriod) {
+      if (
+        BigNumber.from(Date.now()) <= migrationPeriod.add(challengeDuration)
+      ) {
+        validChallengePeriod = true;
+        break;
+      }
+    } else {
+      break;
+    }
+  }
+  if (!validChallengePeriod) {
+    if (reminder) {
+      if (
+        BigNumber.from(Date.now()) >= maturityDate &&
+        BigNumber.from(Date.now()) <= maturityDate.add(challengeDuration)
+      ) {
+        validChallengePeriod = true;
+      }
+    }
+  }
+
+  if (activationDate.toString() !== "0" && address == contractee) {
+    return (
+      <>
+        <StepLabel>{"Cancel or Challenge Agreement"}</StepLabel>
+        <StepContent>
+          <Grid container pt={2}>
+            <Grid item xs={2}></Grid>
+            <Grid item xs>
+              <Alert severity="error">
+                <AlertTitle>
+                  These actions are final and cannot be undone.
+                </AlertTitle>
+                <List>
+                  <ListItem>
+                    <Box>
+                      <Typography variant="subtitle2">
+                        Cancel Agreement
+                      </Typography>
+                      If you cancel the agreement the budget for the days worked
+                      will be transferred to the contractor and you will be
+                      penalized. The rest of the budget will be returned to you.
+                      <br />
+                      <br />
+                    </Box>
+                  </ListItem>
+                  <ListItem>
+                    <Box>
+                      <Typography variant="subtitle2">
+                        Challenge Agreement
+                      </Typography>
+                      Once the actual sprint is done you will have 5 days to
+                      challenge the contractor's work. The dispute will be
+                      submitted to the Contractful DAO and the most justice
+                      course of action will be taken.
+                    </Box>
+                  </ListItem>
+                </List>
+              </Alert>
+              {
+                <Stack pt={2} spacing={2}>
+                  <Button
+                    variant="contained"
+                    color="error"
+                    size="large"
+                    startIcon={<NotInterested />}
+                    fullWidth
+                    onClick={() => {
+                      showConfirmDialog({
+                        dialogTitle:
+                          "Are you sure you want to cancel the agreement?",
+                        dialogBody:
+                          "The budget for the days worked will be transferred to the contractor and you will be penalized.",
+                        cancelCaption: "Keep Agreement",
+                        okCaption: "Cancel Agreement",
+                        okFunc: () => {
+                          handleConsentToAgreement();
+                        },
+                      });
+                    }}
+                  >
+                    Cancel agreement
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="error"
+                    size="large"
+                    startIcon={<NotInterested />}
+                    fullWidth
+                    disabled={!validChallengePeriod}
+                    onClick={() => {
+                      showConfirmDialog({
+                        dialogTitle:
+                          "Are you sure you want challenge the sprint?",
+                        dialogBody:
+                          "The dispute will be submitted to the Contractful DAO. Both parties will have the opportunity to present their case. At the end the most justice course of action will be taken.",
+                        cancelCaption: "Cancel",
+                        okCaption: "Challenge Sprint",
+                        okFunc: () => {
+                          console.log("DEBUG In okFunc");
+                        },
+                      });
+                    }}
+                  >
+                    Challenge sprint
+                  </Button>
+                </Stack>
+              }
+            </Grid>
+            <Grid item xs={3}></Grid>
+          </Grid>
+        </StepContent>
+      </>
+    );
+  }
+  return <></>;
+};
+
+export default ActionSection;
--- contractful-app/pages/review/[agreementId].tsx
+++ contractful-app/pages/review/[agreementId].tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 
@@ -8,24 +7,19 @@ import {
   Chip,
   Container,
   CssBaseline,
-  Grid,
-  Stack,
-  Typography,
-  Stepper,
-  Step,
-  StepLabel,
-  StepContent,
-  Alert,
-  AlertTitle,
   Dialog,
-  DialogTitle,
+  DialogActions,
   DialogContent,
   DialogContentText,
-  DialogActions
+  DialogTitle,
+  Grid,
+  Stack,
+  Step,
+  StepContent,
+  StepLabel,
+  Stepper,
+  Typography,
 } from "@mui/material";
-
-import BrushIcon from "@mui/icons-material/Brush";
-import NotInterested from "@mui/icons-material/NotInterested"
 
 import ContractfulFooter from "../../components/Footer";
 import ContractfulHeader from "../../components/Header";
@@ -37,6 +31,7 @@ import { useAccount, useWaitForTransaction } from "wagmi";
 import ActivateAgreementStatus from "../../components/ActivateAgreement/ActivateAgreementStatus";
 import ContractfulAgreementSummary from "../../components/AgreementSummary";
 import SnackbarAlert from "../../components/general/SnackbarAlert";
+import ActionSection from "../../components/Review/ActionSection";
 import TrafficLight from "../../components/TrafficLight";
 import useActivateAgreement from "../../hooks/useActivateAgreement";
 import useGetAgreementParameters from "../../hooks/useGetAgreementParameters";
@@ -52,9 +47,6 @@ const ReviewHiringAgreement: NextPage = () => {
     "success" | "info" | "warning" | "error"
   >();
   const [openSnackbarAlert, setOpenSnackbarAlert] = useState<boolean>(false);
-
-  const useCase =
-    "useCase" in router.query ? router.query["useCase"] : "consent";
 
   const { agreementId } = router.query;
 
@@ -118,61 +110,62 @@ const ReviewHiringAgreement: NextPage = () => {
   };
 
   // Helper (e.g. completion state of the Stepper)
-  
+
   const isWalletConnected = () => {
-    return (address !== undefined)
-  }
+    return address !== undefined;
+  };
 
   // State of ConfirmDialog
 
-  const [confirmOpen, setConfirmOpen] = React.useState(false)
-  const confirmDialogCancel = () =>{
-    setConfirmOpen(false)
-  }
+  const [confirmOpen, setConfirmOpen] = useState<{
+    dialogTitle: string;
+    dialogBody: string;
+    cancelCaption: string;
+    okCaption: string;
+    okFunc: () => {};
+  }>();
+
+  const confirmDialogCancel = () => {
+    setConfirmOpen(undefined);
+  };
   const confirmDialogOk = () => {
-    setConfirmOpen(false)
-    confirmOpen['okFunc']()
-  }
-  const showConfirmDialog = (data) => {
-    setConfirmOpen(data)
-  }
+    setConfirmOpen(undefined);
+    confirmOpen?.["okFunc"]();
+  };
+  const showConfirmDialog = (data: any) => {
+    setConfirmOpen(data);
+  };
 
   return (
     <>
       <ContractfulHtmlHead />
       <CssBaseline />
       <ContractfulHeader />
-      DEBUG useCase = {useCase}
-      <Container sx={{
-          mt:4
+      <Container
+        sx={{
+          mt: 4,
         }}
       >
-
         <Stepper
           activeStep={isWalletConnected() ? 1 : 0}
           orientation="vertical"
         >
-          <Step
-            key="1"
-            active={true}
-            completed={isWalletConnected()}
-          >
-            <StepLabel>
-              Connect Wallet
-            </StepLabel>
+          <Step key="1" active={true} completed={isWalletConnected()}>
+            <StepLabel>Connect Wallet</StepLabel>
             <StepContent>
               <Grid container pt={2} pl={3}>
                 <Grid item xs>
                   <Stack direction="row">
                     <Typography variant="h6">👋 Welcome</Typography>
-                    {isWalletConnected() &&
-                    <Chip
-                      label={address}
-                      variant="outlined"
-                      sx={{
-                        ml: 2,
-                      }}
-                    />}
+                    {isWalletConnected() && (
+                      <Chip
+                        label={address}
+                        variant="outlined"
+                        sx={{
+                          ml: 2,
+                        }}
+                      />
+                    )}
                   </Stack>
                   <Typography pt={1}>
                     {address !== undefined
@@ -184,14 +177,8 @@ const ReviewHiringAgreement: NextPage = () => {
             </StepContent>
           </Step>
 
-          <Step
-            key="2"
-            active={isWalletConnected()}
-            completed={false}
-          >
-            <StepLabel>
-              Review Hiring Agreement
-            </StepLabel>
+          <Step key="2" active={isWalletConnected()} completed={false}>
+            <StepLabel>Review Hiring Agreement</StepLabel>
             <StepContent>
               <Grid container pt={2} pl={3}>
                 <Grid item xs>
@@ -225,12 +212,17 @@ const ReviewHiringAgreement: NextPage = () => {
                         listStyleType: "none",
                       }}
                     >
-                      <li>✅ Funds have been committed to this Hiring Agreement.</li>
-                      <li>❌ You have not consented to the Hiring Agreement yet.</li>
+                      <li>
+                        ✅ Funds have been committed to this Hiring Agreement.
+                      </li>
+                      <li>
+                        ❌ You have not consented to the Hiring Agreement yet.
+                      </li>
                     </ul>
                   </Stack>
                   <Typography pt={1} pb={4}>
-                    Please review all details of your Hiring Agreement carefully.
+                    Please review all details of your Hiring Agreement
+                    carefully.
                   </Typography>
 
                   <ContractfulAgreementSummary
@@ -241,73 +233,20 @@ const ReviewHiringAgreement: NextPage = () => {
             </StepContent>
           </Step>
 
-          <Step
-            key="3"
-            active={isWalletConnected()}
-            completed={false}
-          >
-            <StepLabel>
-              {"Choose to consent to the Hiring Agreement"}
-            </StepLabel>
-            <StepContent>
-              <Grid container pt={2}>
-                <Grid item xs={2}></Grid>
-                <Grid item xs>
-                  <Alert severity="info">
-                    <AlertTitle>Please review the Agreement carefully.</AlertTitle>
-                    If all details are correct, you can continue to consent to the
-                    Agreement.
-                  </Alert>
-                  <Stack pt={2} spacing={2}>
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      size="large"
-                      startIcon={<BrushIcon />}
-                      fullWidth
-                      onClick={() => {
-                        showConfirmDialog({
-                          dialogTitle: 'Are you sure you want to consent to the Hiring Agreement?',
-                          dialogBody: 'The underlying Smart Contract becomes active by consenting to the Agreement. The Agreement is binding for both parties.',
-                          cancelCaption: 'Cancel',
-                          okCaption: 'Consent to Agreement',
-                          okFunc: (() => {
-                            handleConsentToAgreement()
-                          })
-                        })
-                        }}
-                    >
-                      Consent to the Agreement
-                    </Button>
-                    <Button
-                        variant="contained"
-                        color="error"
-                        size="large"
-                        startIcon={<NotInterested />}
-                        fullWidth
-                        onClick={() => {
-                          showConfirmDialog({
-                            dialogTitle: 'Are you sure you want to decline the Hiring Agreement?',
-                            dialogBody: 'Declining the Hiring Agreement invalidates the Smart Contract to which the budget has been transfered. Be very sure you really want to decline the Hiring Agreement.',
-                            cancelCaption: 'Cancel',
-                            okCaption: 'Really decline Agreement',
-                            okFunc: (() => {
-                              console.log('DEBUG In okFunc')
-                            })
-                          })
-                          }}
-                        >
-                        Decline Agreement
-                    </Button>
-                  </Stack>
-                </Grid>
-                <Grid item xs={3}></Grid>
-              </Grid>
-            </StepContent>
+          <Step key="3" active={isWalletConnected()} completed={false}>
+            {/* challengeDuration is hardcoded for now */}
+            <ActionSection
+              agreementParameters={agreementParameters}
+              challengeDuration={BigNumber.from(24 * 60 * 60 * 5)}
+              confirmOpen={confirmOpen}
+              confirmDialogOk={confirmDialogOk}
+              confirmDialogCancel={confirmDialogCancel}
+              showConfirmDialog={showConfirmDialog}
+              handleConsentToAgreement={handleConsentToAgreement}
+            />
           </Step>
         </Stepper>
       </Container>
-
       <ActivateAgreementStatus
         open={openActivateAgreementStatusDialog}
         onClose={(e) => handleCloseActivateAgreementStatuses(e)}
@@ -333,31 +272,37 @@ const ReviewHiringAgreement: NextPage = () => {
           }
         />
       </Box>
-      <ContractfulFooter />
 
       <Dialog
-          open={confirmOpen}
-          onClose={confirmDialogCancel}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
+        open={confirmOpen ? true : false}
+        onClose={confirmDialogCancel}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
       >
         <DialogTitle id="alert-dialog-title">
-            {confirmOpen['dialogTitle']}
+          {confirmOpen?.["dialogTitle"]}
         </DialogTitle>
         <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-                {confirmOpen['dialogBody']}
-            </DialogContentText>
+          <DialogContentText id="alert-dialog-description">
+            {confirmOpen?.["dialogBody"]}
+          </DialogContentText>
         </DialogContent>
         <DialogActions>
-            <Button onClick={confirmDialogCancel} variant="contained" color="error" autoFocus>
-                {confirmOpen['cancelCaption']}
-            </Button>
-            <Button onClick={confirmDialogOk}>
-                {confirmOpen['okCaption']}
-            </Button>
+          <Button
+            onClick={confirmDialogCancel}
+            variant="contained"
+            color="error"
+            autoFocus
+          >
+            {confirmOpen?.["cancelCaption"]}
+          </Button>
+          <Button onClick={confirmDialogOk}>
+            {confirmOpen?.["okCaption"]}
+          </Button>
         </DialogActions>
       </Dialog>
+
+      <ContractfulFooter />
     </>
   );
 };
